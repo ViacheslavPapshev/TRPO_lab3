c00556a9ffd2d03c3d3024b6b040acde6d1b686e
ViacheslavPapshev <145838726+ViacheslavPapshev@users.noreply.github.com>
2024-03-02 13:09:48 +0300

    Создание консольного приложения

--- ConsoleApp_lab_3/Program.cs
+++ ConsoleApp_lab_3/Program.cs
@@ -1,1 +1,10 @@
-﻿
+﻿using ClassLibrary_lab_3;
+double s1, s2, h;
+
+Console.WriteLine("Введите s1, s2, h усеченного конуса");
+s1 = Convert.ToDouble(Console.ReadLine());
+s2 = Convert.ToDouble(Console.ReadLine());
+h = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Объем усеченного конуса =  ");
+Console.Write(Class1.VolumeCon(s1, s2, h));
