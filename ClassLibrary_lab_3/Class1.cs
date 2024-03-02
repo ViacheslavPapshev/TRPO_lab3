caee9186e49841078abc7e426e7f1fbe492d5188
ViacheslavPapshev <145838726+ViacheslavPapshev@users.noreply.github.com>
2024-03-02 10:27:41 +0300

    Добавил формулу

--- ClassLibrary_lab_3/Class1.cs
+++ ClassLibrary_lab_3/Class1.cs
@@ -2,6 +2,9 @@
 {
     public class Class1
     {
-
+        static public double VolumeCon(double s1, double s2, double h)
+        {
+            return 1.0 / 3 * (s1 + s2 + Math.Pow(s1 * s2, 0.5));
+        }
     }
 }
