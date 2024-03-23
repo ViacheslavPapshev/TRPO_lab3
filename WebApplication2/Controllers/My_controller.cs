9b80bb4ae3685bb5e92d9032d9b3c93ac74a1ff0
ViacheslavPapshev <145838726+ViacheslavPapshev@users.noreply.github.com>
2024-03-23 19:48:49 +0300

    5Лаба

---
+++ WebApplication2/Controllers/My_controller.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication2.Models;
+
+namespace WebApplication2.Controllers
+{
+    public class My_controller : Controller
+    {
+
+        
+        public ActionResult My_view(int id)
+        {
+            var vm = new Presentation_Model();
+            return View(vm);
+        }
+
+ 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult My_view(int id, IFormCollection collection)
+        {
+            //try
+            //{
+            //    return RedirectToAction(nameof(Index));
+            //}
+            //catch
+            //{
+            //    return View();
+            //}
+            var vm = new Presentation_Model();
+            return View(vm);
+        }
+
+
+        
+    }
+}
---
+++ WebApplication2/Models/Presentation Model.cs
@@ -0,0 +1,53 @@
+﻿using ClassLibrary_lab_3;
+
+namespace WebApplication2.Models
+{
+    public class Presentation_Model
+    {
+        private double result;
+
+        private double square_1;
+
+        private double square_2;
+
+        private double height;
+
+        public double Height
+        {
+            get { return height; }
+            set
+            {
+                height = value;
+                Result = Class1.VolumeCon(height, square_1, square_2);
+
+            }
+        }
+
+        public double Square_1
+        {
+            get { return square_1; }
+            set
+            {
+                square_1 = value;
+                Result = Class1.VolumeCon(height, square_1, square_2);
+            }
+        }
+
+        public double Square_2
+        {
+            get { return square_2; }
+            set
+            {
+                Result = Class1.VolumeCon(height, square_1, square_2);
+            }
+        }
+        public double Result
+        {
+            get { return result; }
+            set
+            {
+                result = value;
+            }
+        }
+    }
+}
